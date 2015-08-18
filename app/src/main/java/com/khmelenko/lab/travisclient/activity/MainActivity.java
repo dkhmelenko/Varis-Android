37b032957b8966055c7db746212544b2ad8737cb
Dmytro Khmelenko <dmitriy-broker@rambler.ru>
2015-08-18 22:41:48 +0200

    Navigation drawer replaced with NavigationView

--- app/src/main/java/com/khmelenko/lab/travisclient/activity/MainActivity.java
+++ app/src/main/java/com/khmelenko/lab/travisclient/activity/MainActivity.java
@@ -1,24 +1,21 @@
 package com.khmelenko.lab.travisclient.activity;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ListView;
 
 import com.khmelenko.lab.travisclient.R;
-import com.khmelenko.lab.travisclient.adapter.DrawerAdapter;
 import com.khmelenko.lab.travisclient.model.DrawerItem;
 
 import java.util.ArrayList;
@@ -35,10 +32,10 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     @Bind(R.id.drawer_layout)
-    DrawerLayout mDrawerLayout;
+    DrawerLayout drawerLayout;
 
-    @Bind(R.id.left_drawer)
-    ListView mDrawerList;
+    @Bind(R.id.content)
+    View content;
 
     private List<DrawerItem> mDrawerItems;
 
@@ -53,40 +50,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        mDrawerItems = generateDrawerItems();
-
-        DrawerAdapter adapter = new DrawerAdapter(this, mDrawerItems);
-        mDrawerList.setAdapter(adapter);
-        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
-                handleDrawerItemSelection(pos);
-            }
-        });
-
-        mTitle = mDrawerTitle = getTitle();
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
-
-            /** Called when a drawer has settled in a completely closed state. */
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-//                getActionBar().setTitle(mTitle);
-            }
-
-            /** Called when a drawer has settled in a completely open state. */
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-//                getActionBar().setTitle(mDrawerTitle);
-            }
-        };
-
-        // Set the drawer toggle as the DrawerListener
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-
-//        getActionBar().setDisplayHomeAsUpEnabled(true);
-//        getActionBar().setHomeButtonEnabled(true);
+        initToolbar();
+        setupDrawerLayout();
 
 
         // TODO
@@ -100,74 +65,40 @@ public class MainActivity extends AppCompatActivity {
 //        });
     }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        mDrawerToggle.syncState();
+    private void initToolbar() {
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
+    private void setupDrawerLayout() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-    /**
-     * Generates the list of items for Navigation Drawer
-     *
-     * @return List of navigation drawer items
-     */
-    private List<DrawerItem> generateDrawerItems() {
-        List<DrawerItem> items = new ArrayList<>();
-
-        DrawerItem item = new DrawerItem(R.drawable.login_icon, getString(R.string.navigation_drawer_login));
-        items.add(item);
-
-        DrawerItem logout = new DrawerItem(R.drawable.login_icon, getString(R.string.navigation_drawer_logout));
-        items.add(logout);
-
-        // TODO Add more items
-
-        return items;
-    }
-
-    /**
-     * Handles selection of the drawer list
-     *
-     * @param position Item position
-     */
-    private void handleDrawerItemSelection(int position) {
-// Create a new fragment and specify the planet to show based on position
-        // TODO
-
-//        Fragment fragment = new PlanetFragment();
-//        Bundle args = new Bundle();
-//        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
-//        fragment.setArguments(args);
-//
-//        // Insert the fragment by replacing any existing fragment
-//        FragmentManager fragmentManager = getFragmentManager();
-//        fragmentManager.beginTransaction()
-//                .replace(R.id.content_frame, fragment)
-//                .commit();
-
-        // Highlight the selected item, update the title, and close the drawer
-        mDrawerList.setItemChecked(position, true);
-        setTitle(mDrawerItems.get(position).getTitle());
-        mDrawerLayout.closeDrawer(mDrawerList);
-    }
-
-    @Override
-    public void setTitle(CharSequence title) {
-        mTitle = title;
-//        getActionBar().setTitle(title);
+        NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
+        view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override public boolean onNavigationItemSelected(MenuItem menuItem) {
+                // TODO Handle selection from navigation view
+                Snackbar.make(content, menuItem.getTitle() + " pressed", Snackbar.LENGTH_LONG).show();
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
