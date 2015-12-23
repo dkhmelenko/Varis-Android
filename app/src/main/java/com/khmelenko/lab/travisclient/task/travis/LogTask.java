7e81bfa536df48a668f2cd2e4146eca36299417d
Dmytro Khmelenko <dmitriy-broker@rambler.ru>
2015-12-23 22:45:41 +0100

    Implement loading log file without authorization

--- app/src/main/java/com/khmelenko/lab/travisclient/task/travis/LogTask.java
+++ app/src/main/java/com/khmelenko/lab/travisclient/task/travis/LogTask.java
@@ -37,10 +37,17 @@ public final class LogTask extends Task<String> {
         String redirectUrl = "";
 
         try {
-            if(TextUtils.isEmpty(mAuth)) {
-                mRestClient.getRawApiService().getLog(String.valueOf(mJobId));
+            Response response;
+            if (TextUtils.isEmpty(mAuth)) {
+                response = mRestClient.getRawApiService().getLog(String.valueOf(mJobId));
             } else {
-                mRestClient.getRawApiService().getLog(mAuth, String.valueOf(mJobId));
+                response = mRestClient.getRawApiService().getLog(mAuth, String.valueOf(mJobId));
+            }
+
+            // in case of success just return the url. It means that the log file can be accessed
+            // by this url already
+            if (response.getStatus() == 200) {
+                redirectUrl = response.getUrl();
             }
 
         } catch (TaskException exception) {
