ad60a238f0b7631fe0f437dea69e73780c61bb22
Florian Dreier <dreier.florian@gmail.com>
2017-02-03 16:56:47 +0100

    Implemented animation for started state

--- app/src/main/java/com/khmelenko/lab/travisclient/converter/BuildStateHelper.java
+++ app/src/main/java/com/khmelenko/lab/travisclient/converter/BuildStateHelper.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
 import android.support.v4.content.ContextCompat;
 
 import com.khmelenko.lab.travisclient.R;
@@ -65,8 +66,14 @@ public final class BuildStateHelper {
 
         switch (state) {
             case STATE_CREATED:
-            case STATE_STARTED:
                 return ContextCompat.getDrawable(context, R.drawable.ic_build_state_created_16dp);
+            case STATE_STARTED:
+                AnimatedVectorDrawableCompat drawable = AnimatedVectorDrawableCompat
+                        .create(context, R.drawable.ic_build_state_started_16dp);
+                if (drawable != null) {
+                    drawable.start();
+                }
+                return drawable;
             case STATE_PASSED:
                 return ContextCompat.getDrawable(context, R.drawable.ic_build_state_passed_16dp);
             case STATE_CANCELED:
