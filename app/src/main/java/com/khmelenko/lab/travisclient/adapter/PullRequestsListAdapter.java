0cb41707218f00d1bd7a76656114ef81e4fa76c1
Florian Dreier <dreier.florian@gmail.com>
2017-02-16 15:51:56 +0100

    Replaced BranchViewHolder with BuildViewHolder in PullRequestsListAdapter

--- app/src/main/java/com/khmelenko/lab/travisclient/adapter/PullRequestsListAdapter.java
+++ app/src/main/java/com/khmelenko/lab/travisclient/adapter/PullRequestsListAdapter.java
@@ -6,24 +6,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.khmelenko.lab.travisclient.R;
+import com.khmelenko.lab.travisclient.adapter.viewholder.BuildViewHolder;
 import com.khmelenko.lab.travisclient.network.response.Build;
 import com.khmelenko.lab.travisclient.network.response.Commit;
 import com.khmelenko.lab.travisclient.network.response.RequestData;
 import com.khmelenko.lab.travisclient.network.response.Requests;
-import com.khmelenko.lab.travisclient.widget.BuildView;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
 
 /**
  * List adapter for Pull requests
  *
  * @author Dmytro Khmelenko
  */
-public final class PullRequestsListAdapter extends RecyclerView.Adapter<PullRequestsListAdapter.BranchViewHolder> {
+public final class PullRequestsListAdapter extends RecyclerView.Adapter<BuildViewHolder> {
 
     private Requests mRequests;
     private List<RequestData> mPullRequests;
@@ -46,20 +43,20 @@ public final class PullRequestsListAdapter extends RecyclerView.Adapter<PullRequ
     }
 
     @Override
-    public BranchViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public BuildViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_pull_request, parent, false);
-        return new BranchViewHolder(v);
+        return new BuildViewHolder(v, mListener);
     }
 
     @Override
-    public void onBindViewHolder(BranchViewHolder holder, int position) {
+    public void onBindViewHolder(BuildViewHolder holder, int position) {
         if (mRequests != null) {
             RequestData request = mPullRequests.get(position);
-            bindPullRequest(holder, position, request);
+            bindPullRequest(holder, request);
         }
     }
 
-    private void bindPullRequest(BranchViewHolder holder, int position, RequestData request) {
+    private void bindPullRequest(BuildViewHolder holder, RequestData request) {
         Commit relatedCommit = null;
         for (Commit commit : mRequests.getCommits()) {
             if (request.getCommitId() == commit.getId()) {
@@ -88,29 +85,4 @@ public final class PullRequestsListAdapter extends RecyclerView.Adapter<PullRequ
         return mPullRequests.size();
     }
 
-    /**
-     * Viewholder class
-     */
-    class BranchViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-
-        @Bind(R.id.item_pull_request_card_view)
-        View mParent;
-
-        @Bind(R.id.item_pull_request_data)
-        BuildView mBuildView;
-
-        BranchViewHolder(View itemView) {
-            super(itemView);
-            ButterKnife.bind(this, itemView);
-            itemView.setClickable(true);
-            mParent.setOnClickListener(this);
-        }
-
-        @Override
-        public void onClick(View view) {
-            if (mListener != null) {
-                mListener.onItemSelected(getLayoutPosition());
-            }
-        }
-    }
 }
