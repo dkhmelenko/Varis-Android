3f9cb98d5b347df6a4270a17a5a1d0993b6956e9
Dmytro Khmelenko <dmitriy-broker@rambler.ru>
2017-06-03 15:59:33 +0200

    Update RawClient

--- app/src/main/java/com/khmelenko/lab/varis/network/retrofit/raw/RawClientRx.java
+++ app/src/main/java/com/khmelenko/lab/varis/network/retrofit/raw/RawClientRx.java
@@ -3,11 +3,20 @@ package com.khmelenko.lab.varis.network.retrofit.raw;
 import com.khmelenko.lab.varis.storage.AppSettings;
 
 import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
 
+import io.reactivex.Single;
+import io.reactivex.SingleEmitter;
+import io.reactivex.SingleOnSubscribe;
+import io.reactivex.annotations.NonNull;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
+import retrofit2.Converter;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
 
 /**
@@ -17,10 +26,10 @@ import retrofit2.Retrofit;
  */
 public class RawClientRx {
 
-    private final Retrofit mRetrofit;
+    private Retrofit mRetrofit;
     private final OkHttpClient mHttpClient;
 
-    private RawApiService mRawApiService;
+    private RawApiServiceRx mRawApiService;
 
     public RawClientRx(Retrofit retrofit, OkHttpClient okHttpClient) {
         mRetrofit = retrofit;
@@ -36,10 +45,26 @@ public class RawClientRx {
      * @param newEndpoint New endpoint
      */
     public void updateEndpoint(String newEndpoint) {
-        Retrofit retrofit = mRetrofit.newBuilder()
+        mRetrofit = new Retrofit.Builder()
                 .baseUrl(newEndpoint)
+                .addConverterFactory(new Converter.Factory() {
+                    @Override
+                    public Converter<ResponseBody, ?> responseBodyConverter(Type type, Annotation[] annotations, Retrofit retrofit) {
+                        if (String.class.equals(type)) {
+                            return new Converter<ResponseBody, String>() {
+                                @Override
+                                public String convert(ResponseBody value) throws IOException {
+                                    return value.string();
+                                }
+                            };
+                        }
+                        return null;
+                    }
+                })
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(mHttpClient)
                 .build();
-        mRawApiService = retrofit.create(RawApiService.class);
+        mRawApiService = mRetrofit.create(RawApiServiceRx.class);
     }
 
     /**
@@ -47,7 +72,7 @@ public class RawClientRx {
      *
      * @return Raw API service
      */
-    public RawApiService getApiService() {
+    public RawApiServiceRx getApiService() {
         return mRawApiService;
     }
 
@@ -57,13 +82,24 @@ public class RawClientRx {
      * @param url URL for request
      * @return Response
      */
-    public Response singleRequest(String url) throws IOException {
+    public Single<Response> singleRequest(String url) {
 
         Request request = new Request.Builder()
                 .url(url)
                 .build();
 
-        return mHttpClient.newCall(request).execute();
+        return Single.create(new SingleOnSubscribe<Response>() {
+            @Override
+            public void subscribe(@NonNull SingleEmitter<Response> e) throws Exception {
+                Response response = mHttpClient.newCall(request).execute();
+                e.onSuccess(response);
+            }
+        });
+    }
+
+    public String getLogUrl(Long jobId) {
+        String logUrl = String.format("%sjobs/%d/log", mRetrofit.baseUrl(), jobId);
+        return logUrl;
     }
 
 }
