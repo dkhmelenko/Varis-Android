b39d2b784ed82b68130f4f59c4b146e2a123b08e
Dmytro Khmelenko <dmitriy-broker@rambler.ru>
2018-06-16 11:14:32 +0200

    Fix listener in repo list adapter

--- app-v3/src/main/java/com/khmelenko/lab/varis/repositories/RepoListAdapter.kt
+++ app-v3/src/main/java/com/khmelenko/lab/varis/repositories/RepoListAdapter.kt
@@ -4,7 +4,6 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.khmelenko.lab.varis.R
-import com.khmelenko.lab.varis.adapter.OnListItemListener
 import com.khmelenko.lab.varis.adapter.viewholder.BuildViewHolder
 import com.khmelenko.lab.varis.network.response.Repo
 
@@ -14,17 +13,17 @@ import com.khmelenko.lab.varis.network.response.Repo
  * @author Dmytro Khmelenko
  */
 class RepoListAdapter(
-        private val mRepos: List<Repo>,
-        private val mListener: OnListItemListener
+        private val repos: List<Repo>,
+        private val listener: (position: Int) -> Unit
 ) : RecyclerView.Adapter<BuildViewHolder>() {
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): BuildViewHolder {
         val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_build_view, viewGroup, false)
-        return BuildViewHolder(v, mListener)
+        return BuildViewHolder(v, listener)
     }
 
     override fun onBindViewHolder(repoViewHolder: BuildViewHolder, i: Int) {
-        val repo = mRepos[i]
+        val repo = repos[i]
         repoViewHolder.mBuildView.setTitle(repo.slug)
         repoViewHolder.mBuildView.setStateIndicator(repo.lastBuildState)
         repoViewHolder.mBuildView.setFinishedAt(repo.lastBuildFinishedAt)
@@ -32,6 +31,6 @@ class RepoListAdapter(
     }
 
     override fun getItemCount(): Int {
-        return mRepos.size
+        return repos.size
     }
 }
