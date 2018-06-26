1af161cd4810ae7a5abac67eb06f7a364af5bd2e
Dmytro Khmelenko <dmitriy-broker@rambler.ru>
2018-06-26 23:12:59 +0200

    Convert class Constants to Kotlin

---
+++ app-v3/src/main/java/com/khmelenko/lab/varis/common/Constants.kt
@@ -0,0 +1,25 @@
+package com.khmelenko.lab.varis.common
+
+/**
+ * Application constant values
+ *
+ * @author Dmytro Khmelenko
+ */
+object Constants {
+
+    /**
+     * URL for open source projects
+     */
+    const val OPEN_SOURCE_TRAVIS_URL = "https://api.travis-ci.org"
+
+    /**
+     * URL for private projects
+     */
+    const val PRIVATE_TRAVIS_URL = "https://api.travis-ci.com"
+
+    /**
+     * URL for Github access
+     */
+    const val GITHUB_URL = "https://api.github.com"
+
+}
