5f6df209ec9e1bf0f54dc1ceeb64eb9024cc4b9c
Dmytro Khmelenko <dmitriy-broker@rambler.ru>
2018-06-29 00:11:52 +0200

    Convert BuildHistoryFragment to Kotlin

---
+++ app-v3/src/main/java/com/khmelenko/lab/varis/repodetails/BuildHistoryFragment.kt
@@ -0,0 +1,127 @@
+package com.khmelenko.lab.varis.repodetails
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import butterknife.ButterKnife
+import com.khmelenko.lab.varis.R
+import com.khmelenko.lab.varis.network.response.BuildHistory
+import kotlinx.android.synthetic.main.fragment_list_refreshable.list_refreshable_recycler_view
+import kotlinx.android.synthetic.main.fragment_list_refreshable.list_refreshable_swipe_view
+import kotlinx.android.synthetic.main.fragment_list_refreshable.progressbar
+import kotlinx.android.synthetic.main.view_empty.empty_text
+
+/**
+ * Repository Build history
+ *
+ * @author Dmytro Khmelenko
+ */
+class BuildHistoryFragment : Fragment() {
+
+    private lateinit var buildListAdapter: BuildListAdapter
+    private var buildHistory: BuildHistory? = null
+
+    private var listener: BuildHistoryListener? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_list_refreshable, container, false)
+        ButterKnife.bind(this, view)
+
+        list_refreshable_recycler_view.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(context)
+        list_refreshable_recycler_view.layoutManager = layoutManager
+
+        buildListAdapter = BuildListAdapter(buildHistory, { position ->
+            val build = buildHistory!!.builds[position]
+            listener?.onBuildSelected(build.id)
+        })
+        list_refreshable_recycler_view.adapter = buildListAdapter
+
+        list_refreshable_swipe_view.setColorSchemeResources(R.color.swipe_refresh_progress)
+        list_refreshable_swipe_view.setOnRefreshListener { listener?.onReloadBuildHistory() }
+
+        progressbar.visibility = View.VISIBLE
+
+        return view
+    }
+
+
+    /**
+     * Checks whether data existing or not
+     */
+    private fun checkIfEmpty() {
+        empty_text.setText(R.string.repo_details_builds_empty)
+        if (buildHistory == null || buildHistory!!.builds.isEmpty()) {
+            empty_text.visibility = View.VISIBLE
+        } else {
+            empty_text.visibility = View.GONE
+        }
+    }
+
+    override fun onAttach(activity: Context?) {
+        super.onAttach(activity)
+        try {
+            listener = activity as BuildHistoryListener?
+        } catch (e: ClassCastException) {
+            throw ClassCastException(activity!!.toString() + " must implement BuildHistoryListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    /**
+     * Sets build history
+     *
+     * @param buildHistory Build history
+     */
+    fun setBuildHistory(buildHistory: BuildHistory?) {
+        list_refreshable_swipe_view.isRefreshing = false
+        progressbar.visibility = View.GONE
+
+        if (buildHistory != null) {
+            this.buildHistory = buildHistory
+            buildListAdapter.setBuildHistory(this.buildHistory)
+            buildListAdapter.notifyDataSetChanged()
+        }
+
+        checkIfEmpty()
+    }
+
+    /**
+     * Interface for communication with this fragment
+     */
+    interface BuildHistoryListener {
+
+        /**
+         * Handles build selection
+         *
+         * @param buildNumber Build number
+         */
+        fun onBuildSelected(buildNumber: Long)
+
+        /**
+         * Handles reload data request
+         */
+        fun onReloadBuildHistory()
+    }
+
+    companion object {
+
+        /**
+         * Creates new instance of the fragment
+         *
+         * @return Fragment instance
+         */
+        fun newInstance(): BuildHistoryFragment {
+            return BuildHistoryFragment()
+        }
+    }
+}
---
+++ app-v3/src/main/java/com/khmelenko/lab/varis/repodetails/BuildListAdapter.kt
@@ -0,0 +1,47 @@
+package com.khmelenko.lab.varis.repodetails
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+
+import com.khmelenko.lab.varis.R
+import com.khmelenko.lab.varis.adapter.OnListItemListener
+import com.khmelenko.lab.varis.adapter.viewholder.BuildViewHolder
+import com.khmelenko.lab.varis.network.response.BuildHistory
+import com.khmelenko.lab.varis.network.response.Commit
+
+/**
+ * Adapter for the list of builds
+ *
+ * @author Dmytro Khmelenko
+ */
+class BuildListAdapter(private var buildHistory: BuildHistory?,
+                       private val listener: (Int) -> Unit) : RecyclerView.Adapter<BuildViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BuildViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_build_view, parent, false)
+        return BuildViewHolder(v, listener)
+    }
+
+    override fun onBindViewHolder(holder: BuildViewHolder, position: Int) {
+        if (buildHistory != null) {
+            val build = buildHistory!!.builds[position]
+            val relatedCommit = buildHistory!!.commits.firstOrNull { it.id == build.commitId }
+            holder.mBuildView.setState(build)
+            holder.mBuildView.setCommit(relatedCommit)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return if (buildHistory != null) buildHistory!!.builds.size else 0
+    }
+
+    /**
+     * Sets build history
+     *
+     * @param buildHistory Build history
+     */
+    fun setBuildHistory(buildHistory: BuildHistory?) {
+        this.buildHistory = buildHistory
+    }
+}
