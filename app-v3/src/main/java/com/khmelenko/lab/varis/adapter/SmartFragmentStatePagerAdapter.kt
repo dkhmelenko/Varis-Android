3f1ba70c4193772e4a26a2b9563ec73611b800d9
Dmytro Khmelenko <dmitriy-broker@rambler.ru>
2018-08-04 00:02:24 +0200

    Convert SmartFragmentStatePagerAdapter to Kotlin

---
+++ app-v3/src/main/java/com/khmelenko/lab/varis/adapter/SmartFragmentStatePagerAdapter.kt
@@ -0,0 +1,38 @@
+package com.khmelenko.lab.varis.adapter
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import android.util.SparseArray
+import android.view.ViewGroup
+
+/**
+ * Extension of FragmentStatePagerAdapter which intelligently caches
+ * all active fragments and manages the fragment lifecycles.
+ * Usage involves extending from SmartFragmentStatePagerAdapter as you would any other PagerAdapter.
+ *
+ * @author Dmytro Khmelenko (d.khmelenko@gmail.com)
+ */
+abstract class SmartFragmentStatePagerAdapter(fragmentManager: FragmentManager) : FragmentStatePagerAdapter(fragmentManager) {
+
+    // Sparse array to keep track of registered fragments in memory
+    private val mRegisteredFragments = SparseArray<Fragment>()
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val fragment = super.instantiateItem(container, position) as Fragment
+        mRegisteredFragments.put(position, fragment)
+        return fragment
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        mRegisteredFragments.remove(position)
+        super.destroyItem(container, position, `object`)
+    }
+
+    /**
+     * Returns the fragment for the position (if instantiated)
+     */
+    fun getRegisteredFragment(position: Int): Fragment {
+        return mRegisteredFragments.get(position)
+    }
+}
